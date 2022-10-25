14f9dc43004834281b2728e21bdb72685afa3a31
G4llifreyan <zeg4llifreyan@gmail.com>
2022-10-25 20:54:32 +0200

    (Better Tiles) Instance Tile

---
+++ Runtime/BetterTiles/InstanceTile.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "2D Bernique Utils/Better Tiles/Instance Tile")]
+public class InstanceTile : Tile {
+
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go) {
+        Debug.Log(go);
+        return base.StartUp(position, tilemap, go);
+    }
+
+}
